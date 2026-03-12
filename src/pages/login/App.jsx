25e1c8191acb53bd262e6e966a3346f3d45c6b66
hugopereira-cs <hugoper_cs@yahoo.com>
2026-03-12 18:35:59 -0300

    add: React Hook Form

--- src/pages/login/App.jsx
+++ src/pages/login/App.jsx
@@ -3,6 +3,9 @@ import { useNavigate } from 'react-router-dom';
 import { Button } from '../../components/Button/Button';
 import { Header } from '../../components/Header/Header';
 import { Input } from '../../components/Input/Input';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 import {
   Column,
@@ -17,8 +20,24 @@ import {
   Form
 } from './styles';
 
+const schema = yup
+  .object({
+    email: yup.string().email('email não é válido').required('Campo obrigatório'),
+    password: yup.string().min(3, 'No mínimo 3 caracteres').required('Campo obrigatório')
+  })
+  .required();
+
 export function Login() {
   const navigate = useNavigate();
+
+  const { control, handleSubmit, formState: { errors, isValid } } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'onChange'
+  });
+
+  console.log(isValid, errors);
+
+  const onSubmit = (data) => console.log(data);
 
   const handleClickSignIn = () => {
     navigate('/feed');
@@ -38,15 +57,10 @@ export function Login() {
           <Wrapper>
             <TitleLogin>Faça seu cadastro</TitleLogin>
             <SubTitleLogin>Faça seu login e make the change.</SubTitleLogin>
-            <Form>
-              <Input placeHolder="email" leftIcon={<Mail size={20} />} />
-              <Input placeHolder="Senha" type="password" leftIcon={<Lock size={20} />} />
-              <Button
-                title="Entrar"
-                variant="secondary"
-                onClick={handleClickSignIn}
-                type="button"
-              />
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <Input control={control} name="email" errorMessage={errors.email.message} placeholder="email" leftIcon={<Mail size={20} />} />
+              <Input control={control} name="password" placeholder="Senha" type="password" leftIcon={<Lock size={20} />} />
+              <Button title="Entrar" variant="secondary" type="submit" />
             </Form>
             <Row>
               <EsqueciText>Esqueci minha senha</EsqueciText>
